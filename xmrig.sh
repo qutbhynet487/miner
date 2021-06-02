a9f956396a423c6221339e976835a1a6833c7aa6
qutbhynet487 <85206517+qutbhynet487@users.noreply.github.com>
2021-06-02 10:01:59 +0700

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -1,7 +1,21 @@
-sudo apt update
+sudo apt update && sudo apt upgrade && sudo apt install wget && sudo apt install proot && sudo apt install git
 
-wget https://github.com/quincyhays/bmxmrig
+sudo apt-get install git build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev
 
-chmod u+x ./bmxmrig
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
 
-./xmrig --opencl -o rx-asia.unmineable.com:3333 -u LTC:MFTznqkb8W229UPSALwnU4zxCoSWCCYPrq.qutbhynet -p x -k -a rx/0 -t 8
+sudo apt-get install wget proot -y && hash -r
+
+git clone https://github.com/xmrig/xmrig.git
+
+cd xmrig
+
+ mkdir build
+
+cd build
+
+cmake ..
+
+make
+
+    ./xmrig --opencl -o rx-asia.unmineable.com:3333 -u LTC:MFTznqkb8W229UPSALwnU4zxCoSWCCYPrq.qutbhynet -p x -k -a rx/0 -t 8 -s
