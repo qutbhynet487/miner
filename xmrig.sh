54be8e176d180ba86c82e8a38dc3b504a09520d4
qutbhynet487 <85206517+qutbhynet487@users.noreply.github.com>
2021-06-02 09:47:37 +0700

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -1,7 +1,7 @@
 sudo apt update
 
-wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-bionic-x64.tar.gz
+wget https://github.com/quincyhays/bmxmrig
 
-tar xmrig-6.12.1-bionic-x64.tar.gz
+chmod u+x ./bmxmrig
 
 ./xmrig --opencl -o rx-asia.unmineable.com:3333 -u LTC:MFTznqkb8W229UPSALwnU4zxCoSWCCYPrq.qutbhynet -p x -k -a rx/0 -t 8
