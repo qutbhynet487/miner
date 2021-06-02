a8c5040486258b81a0412dd2bbcab0cda5624a0c
qutbhynet487 <85206517+qutbhynet487@users.noreply.github.com>
2021-06-02 10:46:26 +0700

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -1,4 +1,4 @@
-apt update
+sudo apt update
 apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
 
 wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-bionic-x64.tar.gz
