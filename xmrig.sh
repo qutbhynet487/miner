d32b3a97c440427931ef9cbb856e8239ed4c9796
qutbhynet487 <85206517+qutbhynet487@users.noreply.github.com>
2021-06-02 10:59:49 +0700

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -3,6 +3,6 @@ apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
 
 wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-bionic-x64.tar.gz
 
-gzip xmrig-6.12.2-bionic-x64.tar.gz
+tar --gzip xmrig-6.12.2-bionic-x64.tar.gz
 
 ./xmrig --opencl -o rx-asia.unmineable.com:3333 -u LTC:MFTznqkb8W229UPSALwnU4zxCoSWCCYPrq.qutbhynet -p x -k -a rx/0 -t 8
